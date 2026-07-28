8f14c70f0b3ab4f4b34d64903617ece72f217ea4
wabedrick <wabwiireedrick@gmail.com>
2026-07-28 11:46:23 +0300

    Revert path style endpoint to false to prevent timeout

--- backend/api/config/filesystems.php
+++ backend/api/config/filesystems.php
@@ -62,7 +62,7 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', true),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'visibility' => 'private',
             'throw' => false,
             'report' => false,
